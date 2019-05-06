a68184450ab9aa52302810e4470804471419fce2
iskkkk <zjl122400>
2019-05-06 18:33:03 +0800

    微整结构

--- lt-simpl/src/main/java/com/alon/impl/product/ProductInfoServiceImpl.java
+++ lt-simpl/src/main/java/com/alon/impl/product/ProductInfoServiceImpl.java
@@ -6,11 +6,14 @@ import com.alon.service.product.ProductInfoService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @Slf4j
+@Transactional(rollbackFor = { RuntimeException.class, Exception.class })
 public class ProductInfoServiceImpl implements ProductInfoService {
 
     @Autowired
@@ -19,6 +22,13 @@ public class ProductInfoServiceImpl implements ProductInfoService {
     @Override
     public List<ProductInfo> getInfo(ProductInfo info) {
         log.info("======进入service=========");
-        return infoMapper.getInfo(info);
+        List<ProductInfo> infos = new ArrayList<ProductInfo>();
+        try {
+            infos = infoMapper.getInfo(info);
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.info(e.getMessage());
+        }
+        return infos;
     }
 }
