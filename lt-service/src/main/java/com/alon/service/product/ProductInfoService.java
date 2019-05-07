b66fae617de662adcb3dc22e3d1c8c2dae8405cc
iskkkk <zjl122400>
2019-05-07 16:56:32 +0800

    druid连接池配置

--- lt-service/src/main/java/com/alon/service/product/ProductInfoService.java
+++ lt-service/src/main/java/com/alon/service/product/ProductInfoService.java
@@ -6,10 +6,10 @@ import java.util.List;
 
 public interface ProductInfoService {
     /**
+      * 方法表述: 获取商品信息(ctrl+alt+v 快速填充)
       * @Author 一股清风
-      * @Description 获取商品信息
-      * @Date 19:23 2019/5/6
-     * @param       info
+      * @Date 14:43 2019/5/7
+      * @param       info
       * @return java.util.List<com.alon.model.ProductInfo>
     */
     List<ProductInfo> getInfo(ProductInfo info);
