39ac309ee578fd399bc6c1c5c52ede7cac75424d
iskkkk <zjl122400>
2019-05-20 14:06:43 +0800

    test

--- lt-web/src/test/java/com/alon/web/LtWebApplicationTests.java
+++ lt-web/src/test/java/com/alon/web/LtWebApplicationTests.java
@@ -1,16 +1,19 @@
 package com.alon.web;
 
+import com.alon.amqp.model.Employee;
+import com.alon.amqp.producer.MessageProducer;
+import com.alon.common.utils.DBUtil;
 import io.searchbox.client.JestClient;
 import io.searchbox.core.Search;
 import io.searchbox.core.SearchResult;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
@@ -21,6 +24,9 @@ public class LtWebApplicationTests {
 
     @Autowired
     private JestClient jestClient;
+    @Autowired
+    private MessageProducer messageProducer;
+
     @Test
     public void contextLoads() {
         //自定义一个dsl语句
@@ -38,14 +44,32 @@ public class LtWebApplicationTests {
         Search search = new Search.Builder(query).addIndex("forum").addType("article").build();
         try {
             SearchResult result = jestClient.execute(search);
-            List<SearchResult.Hit<Map, Void>> hits = result.getHits(Map.class);
-            for (SearchResult.Hit<Map, Void> hit : hits) {
-                Map map = hit.source;
-                log.info("数据：" + map);
+            if (StringUtils.isBlank(result.getErrorMessage())) {
+                List<SearchResult.Hit<Map, Void>> hits = result.getHits(Map.class);
+                for (SearchResult.Hit<Map, Void> hit : hits) {
+                    Map map = hit.source;
+                    log.info("数据：" + map);
+                }
+            } else {
+                log.error("错误：" + result.getErrorMessage());
             }
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
+            log.error("异常：" + e.getMessage());
         }
     }
 
+    @Test
+    public void testAmqp() {
+        messageProducer.sendMessage(new Employee("3306","Alon",18));
+    }
+
+    @Test
+    public void testDBU() {
+        try {
+            DBUtil.getConn();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
