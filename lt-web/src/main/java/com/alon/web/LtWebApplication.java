33e7907f889694fe3b5d2ffeac43f8a32dc0d72a
iskkkk <zjl122400>
2019-06-05 13:42:05 +0800

    spring task定时任务

--- lt-web/src/main/java/com/alon/web/LtWebApplication.java
+++ lt-web/src/main/java/com/alon/web/LtWebApplication.java
@@ -6,10 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ComponentScan("com.alon")
 @MapperScan("com.alon.mapper.dao")
+@EnableScheduling //开启定时任务
+//@EnableAsync //开启异步任务
 public class LtWebApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
