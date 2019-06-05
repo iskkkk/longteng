2c98ad9d1c2880765b9ef9af97ab0910000b0b94
iskkkk <zjl122400>
2019-06-05 16:54:40 +0800

    update

---
+++ lt-common/src/main/java/com/alon/common/config/LtConfigParams.java
@@ -0,0 +1,21 @@
+package com.alon.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * @ClassName LtConfigParams
+ * @Description 获取配置文件中的参数
+ * @Author zoujiulong
+ * @Date 2019/6/5 15:54
+ * @Version 1.0
+ **/
+@Component
+public class LtConfigParams {
+    @Value("${lt.wx.appId}")
+    public String appId;
+
+    @Value("${lt.wx.appSecret}")
+    public String appSecret;
+
+}
