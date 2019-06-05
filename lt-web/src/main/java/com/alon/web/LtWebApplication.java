0587f81b44d824dbae8ea68951a36c72ed94fcf5
iskkkk <zjl122400>
2019-06-05 16:27:57 +0800

    update

--- lt-web/src/main/java/com/alon/web/LtWebApplication.java
+++ lt-web/src/main/java/com/alon/web/LtWebApplication.java
@@ -6,12 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ComponentScan("com.alon")
 @MapperScan("com.alon.mapper.dao")
-@EnableScheduling //开启定时任务
+//@EnableScheduling //开启定时任务
 //@EnableAsync //开启异步任务
 public class LtWebApplication extends SpringBootServletInitializer {
 
